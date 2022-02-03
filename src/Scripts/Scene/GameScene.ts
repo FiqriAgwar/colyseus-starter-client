2a6cb1c554e0041d807108e19f214b0f7571ad54
Aliffiqri Agwar <fiqri.agwar@gmail.com>
2022-02-03 09:35:08 +0800

    Add spawn coin

--- src/Scripts/Scene/GameScene.ts
+++ src/Scripts/Scene/GameScene.ts
@@ -1,5 +1,5 @@
 import { Client, Room } from "colyseus.js";
-import { GameObjects, Input, Scene, Scenes, Types } from 'phaser';
+import { GameObjects, Input, Scene, Scenes, Types } from "phaser";
 import { SERVER_MSG } from "../Config/ServerMessage";
 import { BattleSchema } from "../Schema/BattleSchema";
 
@@ -12,22 +12,24 @@ export default class GameScene extends Scene {
   player?: Types.Physics.Arcade.ImageWithDynamicBody;
   players!: Map<number, GameObjects.Image>;
 
-  bound = Math.pow(2,12);
+  coins!: Map<number, GameObjects.Image>;
+
+  bound = Math.pow(2, 12);
   cursors!: Types.Input.Keyboard.CursorKeys;
   gameHUD?: Scene;
 
-  constructor(){
-    super('GameScene');
+  constructor() {
+    super("GameScene");
   }
 
   // !* -------- INITIATION -------- *!
 
-  init(){
+  init() {
     this.resetReference();
 
     this.playerId = (Math.floor(Math.random() * 1e5) + Date.now()) % 65000;
 
-    this.client = new Client(`ws://${window.location.hostname}:2567`);
+    this.client = new Client(`ws://${window.location.hostname}:3300`);
 
     this.cameras.main.setBounds(0, 0, this.bound, this.bound);
     this.physics.world.setBounds(0, 0, this.bound, this.bound);
@@ -35,10 +37,11 @@ export default class GameScene extends Scene {
     this.cursors = this.input.keyboard.createCursorKeys();
   }
 
-  resetReference(){
+  resetReference() {
     this.battleRoom = undefined;
     this.player = undefined;
     this.players = new Map<number, GameObjects.Image>();
+    this.coins = new Map<number, GameObjects.Image>();
   }
 
   // !* -------- SETUP -------- *!
@@ -50,9 +53,9 @@ export default class GameScene extends Scene {
 
   async connect() {
     try {
-      this.battleRoom = await this.client.joinOrCreate('battle_room');
-    }
-    catch (error) {
+      this.battleRoom = await this.client.joinOrCreate("battle_room");
+    } catch (error) {
+      console.log(error);
       throw new Error("An Error has been occured");
     }
 
@@ -62,7 +65,7 @@ export default class GameScene extends Scene {
   }
 
   setupBattleRoomEvents() {
-    if(!this.battleRoom){
+    if (!this.battleRoom) {
       return;
     }
 
@@ -74,23 +77,38 @@ export default class GameScene extends Scene {
       this.despawnPlayer(id);
     });
 
+    this.battleRoom.onMessage(SERVER_MSG.COLLECTED, (id: number) => {
+      this.despawnCoin(id);
+    });
+
     this.battleRoom.onStateChange((state: BattleSchema) => {
       state.players.forEach((p) => {
         const { x, y } = p.position;
-        p.isSpawned && this.handlePlayer(p.id, x, y, p.angle);
-      })
-    })
+        p.isSpawned && this.handlePlayer(p.id, x, y, p.angle, p.point);
+      });
+
+      state.collectibles.forEach((c) => {
+        const { x, y } = c.position;
+        this.handleCoin(c.id, x, y);
+      });
+    });
   }
 
-  despawnPlayer(id : number){
+  despawnPlayer(id: number) {
     const player = this.players.get(id);
     player?.destroy();
     this.players.delete(id);
   }
 
-  handlePlayer(id: number, x: number, y: number, angle: number){
-    if(this.players.has(id)){
-      if(id !== this.playerId){
+  despawnCoin(id: number) {
+    const coin = this.coins.get(id);
+    coin?.destroy();
+    this.coins.delete(id);
+  }
+
+  handlePlayer(id: number, x: number, y: number, angle: number, point: number) {
+    if (this.players.has(id)) {
+      if (id !== this.playerId) {
         this.handlePlayerTransform(id, x, y, angle);
       }
 
@@ -98,31 +116,49 @@ export default class GameScene extends Scene {
     }
 
     let player;
-    if(id === this.playerId){
-      player = this.physics.add.image(x, y, 'tanks'); //change to tank after spritesheet finished
+    if (id === this.playerId) {
+      player = this.physics.add.image(x, y, "tanks"); //change to tank after spritesheet finished
       this.player = player;
       this.setupCameraFollow();
       this.setupPlayerHUD();
-    }
-    else {
-      player = this.physics.add.image(x, y, 'tanks'); //change to tank after spritesheet finished
+    } else {
+      player = this.physics.add.image(x, y, "tanks"); //change to tank after spritesheet finished
     }
 
-    if(player){
+    if (player) {
       player.setOrigin(0.5);
-      player.setData('id', id);
-      player.setData('transform', {x, y, angle: 0});
+      player.setData("id", id);
+      player.setData("transform", { x, y, angle: 0 });
+      player.setData("point", point);
       this.players.set(id, player);
+    }
+  }
+
+  handleCoin(id: number, x: number, y: number) {
+    if (this.coins.has(id)) {
+      const coin = this.coins.get(id);
+      coin?.setData("transform", { x, y });
+
+      return;
+    }
+
+    let coin;
+    coin = this.physics.add.image(x, y, "coin");
+    if (coin) {
+      coin.setOrigin(0.5);
+      coin.setData("id", id);
+      coin.setData("transform", { x, y });
+      this.coins.set(id, coin);
     }
   }
 
   handlePlayerTransform(id: number, x: number, y: number, angle: number) {
     const player = this.players.get(id);
-    player?.setData('transform', {x, y, angle});
+    player?.setData("transform", { x, y, angle });
   }
 
   setupCameraFollow() {
-    if(!this.player || !this.battleRoom){
+    if (!this.player || !this.battleRoom) {
       return;
     }
 
@@ -130,25 +166,61 @@ export default class GameScene extends Scene {
   }
 
   setupPlayerHUD() {
-    this.scene.launch('GameHUD', {player: this.player, scene: this});
-    this.gameHUD = this.scene.get('GameHUD');
+    this.scene.launch("GameHUD", {
+      player: this.player,
+      players: this.players,
+      scene: this,
+    });
+    this.gameHUD = this.scene.get("GameHUD");
   }
 
   setupSpawnButton() {
-    const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
-    const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
+    const screenCenterX =
+      this.cameras.main.worldView.x + this.cameras.main.width / 2;
+    const screenCenterY =
+      this.cameras.main.worldView.y + this.cameras.main.height / 2;
+
+    const rectangle = this.add.rectangle(0, 0, 300, 150, 0x069420, 1);
+    const spawnText = this.add.text(0, 0, "START GAME", {
+      fontSize: "32px",
+      wordWrap: { width: 200 },
+      align: "center",
+    });
+    const button = this.add.container(screenCenterX, screenCenterY, [
+      rectangle,
+      spawnText,
+    ]);
+
+    rectangle.setInteractive().on(Input.Events.POINTER_DOWN, () => {
+      if (!this.battleRoom) {
+        return;
+      }
+
+      this.spawnPlayer();
+      button.destroy();
+    });
+  }
+
+  spawnPlayer() {
+    const data = {
+      id: this.playerId,
+      x: Math.floor(Math.random() * this.bound),
+      y: Math.floor(Math.random() * this.bound),
+    };
+
+    this.battleRoom?.send("spawn", data);
   }
 
   setupRestartEvent() {
-    this.input.keyboard.removeListener('keyup-R', this.restartScene, this);
-    this.input.keyboard.once('keyup-R', this.restartScene, this);
+    this.input.keyboard.removeListener("keyup-R", this.restartScene, this);
+    this.input.keyboard.once("keyup-R", this.restartScene, this);
   }
 
   restartScene() {
-    this.goToNextScene('GameScene');
+    this.goToNextScene("GameScene");
   }
 
-  goToNextScene(scene: string, data? : object) {
+  goToNextScene(scene: string, data?: object) {
     this.battleRoom?.leave();
     this.player?.destroy();
     this.players = new Map();
@@ -157,5 +229,82 @@ export default class GameScene extends Scene {
   }
 
   // !* -------- UPDATE CONTROL -------- *!
+  update() {
+    this.updatePlayerPosition(0.333);
 
+    if (!this.player?.active) {
+      return;
+    }
+
+    this.player.setVelocity(0);
+
+    if (this.cursors.left.isDown) {
+      this.player.setAngle(-90).setVelocityX(-200);
+    } else if (this.cursors.right.isDown) {
+      this.player.setAngle(90).setVelocityX(200);
+    }
+
+    if (this.cursors.up.isDown) {
+      this.player.setAngle(0).setVelocityY(-200);
+    } else if (this.cursors.down.isDown) {
+      this.player.setAngle(-180).setVelocityY(200);
+    }
+
+    this.sendPlayerTransform();
+  }
+
+  updatePlayerPosition(percentage: number) {
+    this.players?.forEach((player) => {
+      const id = player.getData("id") as number;
+      if (id === this.playerId || !player) {
+        return;
+      }
+
+      const {
+        x: nX,
+        y: nY,
+        angle: nAngle,
+      } = (player.getData("transform") as {
+        x: number;
+        y: number;
+        angle: number;
+      }) || {};
+
+      const { x, y } = player;
+      player.setPosition(
+        Phaser.Math.Linear(x, nX, percentage),
+        Phaser.Math.Linear(y, nY, percentage)
+      );
+
+      player.setAngle(nAngle || 0);
+    });
+  }
+
+  sendPlayerTransform() {
+    if (!this.player?.active || !this.battleRoom) {
+      return;
+    }
+
+    const data = {
+      x: this.player.x,
+      y: this.player.y,
+      angle: this.player.angle,
+    };
+
+    const lastMove = this.player.getData("lastMove");
+
+    if (
+      !lastMove ||
+      lastMove.x !== data.x ||
+      lastMove.y !== data.y ||
+      lastMove.angle !== data.angle
+    ) {
+      try {
+        this.battleRoom.send("move", data);
+        this.player.setData("lastMove", data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
 }
